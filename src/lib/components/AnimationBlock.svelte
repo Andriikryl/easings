7dd5b1783a69663d7279e9d06b96496b83ad05c9
Andriikryl <andreyk557@gmail.com>
2024-02-21 15:16:55 +0200

    add AnimationBlock component

---
+++ src/lib/components/AnimationBlock.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import MovementEasing from "./MovementEasing.svelte";
+  import WidthEasing from "./WidthEasing.svelte";
+  export let currentEasingType: string | any;
+  export let curveCSS: string;
+</script>
+
+<div class="animation__wrapper">
+  <h3 class="title">animation</h3>
+  <MovementEasing {curveCSS} {currentEasingType} />
+  <WidthEasing {curveCSS} {currentEasingType} />
+</div>
+
+<style>
+  .animation__wrapper {
+    min-width: 500px;
+  }
+  .title {
+    color: #fff;
+  }
+</style>
