a39fd85ca24c3c81c411bdef07347afb7decb2d8
Andriikryl <andreyk557@gmail.com>
2024-02-21 15:41:18 +0200

    add CurentEasingPath component

---
+++ src/lib/components/CurentEasingPath.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  export let startHandleX: number;
+  export let startHandleY: number;
+  export let endHandleX: number;
+  export let endHandleY: number;
+</script>
+
+<path
+  class="current-curve__curve"
+  d="M20,200 C{startHandleX},{startHandleY} {endHandleX},{endHandleY} 120,100"
+/>
+
+<style>
+  .current-curve__curve {
+    fill: none;
+    stroke: #e63946;
+    stroke-width: 2px;
+    pointer-events: none;
+  }
+</style>
