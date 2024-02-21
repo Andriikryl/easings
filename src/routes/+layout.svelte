fb7abe393d0103649cbdd9c274e19ef0c6418d41
Andriikryl <andreyk557@gmail.com>
2024-02-21 14:24:18 +0200

    add layout component

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import Footer from "$lib/components/Footer.svelte";
+  import Header from "$lib/components/Header.svelte";
+  import "../styles/global.scss";
+</script>
+
+<svelte:head>
+  <title>Svelte GUI</title>
+</svelte:head>
+
+<Header />
+<main>
+  <slot />
+</main>
+<Footer />
