7b428fb9bd60bbf39e5a86bdd073303a86a5fe70
Andriikryl <andreyk557@gmail.com>
2024-02-21 19:07:54 +0200

    add EaasingCode component

--- src/lib/components/EasingCode.svelte
+++ src/lib/components/EasingCode.svelte
@@ -8,10 +8,12 @@
 
 <style>
   .current-curve__coordinates {
+    padding: 10px;
     white-space: normal;
     font-size: 0.5rem;
     display: inline-block;
     margin: 0 auto;
+    background-color: #fff;
 
     @media (min-width: 34rem) {
       font-size: 0.7rem;
