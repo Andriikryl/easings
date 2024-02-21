a60a558034ba9a90cdfd3712a0fcec76aca17a00
Andriikryl <andreyk557@gmail.com>
2024-02-21 18:56:24 +0200

    add EasingCode component

---
+++ src/lib/components/EasingCode.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  export let curveCSS: string;
+</script>
+
+<code class="current-curve__coordinates">
+  {curveCSS};
+</code>
+
+<style>
+  .current-curve__coordinates {
+    white-space: normal;
+    font-size: 0.5rem;
+    display: inline-block;
+    margin: 0 auto;
+
+    @media (min-width: 34rem) {
+      font-size: 0.7rem;
+    }
+
+    @media (min-width: 66rem) {
+      font-size: 0.8rem;
+    }
+  }
+</style>
