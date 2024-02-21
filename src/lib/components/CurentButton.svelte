40bb63f1581a0055c98fc92e12f2ec6dd654d212
Andriikryl <andreyk557@gmail.com>
2024-02-21 15:17:19 +0200

    add CurentButton component

---
+++ src/lib/components/CurentButton.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  export let copyToClipboard: () => void;
+  export let copyButtonIcon: string;
+</script>
+
+<button
+  class="current-curve__copy-btn"
+  on:click={copyToClipboard}
+  type="button"
+>
+  <span aria-hidden="true">{copyButtonIcon}</span>
+</button>
+
+<style>
+  .current-curve__copy-btn {
+    min-width: 210px;
+    margin-inline: auto;
+    padding: 12px 16px;
+    border-radius: 8px;
+    border: 2px solid var(--black, #1b1b1b);
+    background: var(--white, #fff);
+    color: var(--black, #1b1b1b);
+    text-align: center;
+    font-family: Patrick Hand;
+    font-size: 22px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 28px;
+    cursor: pointer;
+  }
+</style>
