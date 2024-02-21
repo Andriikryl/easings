89aaaf210b030420a1e8617a496cdb733c096441
Andriikryl <andreyk557@gmail.com>
2024-02-21 15:49:11 +0200

    add CurentRectBeg component

---
+++ src/lib/components/CurentRectBeg.svelte
@@ -0,0 +1,13 @@
+<script></script>
+
+<rect class="current-curve__frame" x="20" y="100" width="100" height="100" />
+
+
+<style>
+      .current-curve__frame {
+      fill: none;
+      stroke: #ffd60a;
+      stroke-width: 1px;
+      pointer-events: none;
+    }
+</style>
