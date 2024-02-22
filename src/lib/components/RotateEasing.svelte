efe3940f2fe8745136da04dc3c95f353a31b5019
Andriikryl <andreyk557@gmail.com>
2024-02-22 13:07:24 +0200

    add RotateEasing component

---
+++ src/lib/components/RotateEasing.svelte
@@ -0,0 +1,93 @@
+<script lang="ts">
+  import LiniarEasing from "./LiniarEasing.svelte";
+  export let currentEasingType: string | any;
+  export let curveCSS: string;
+</script>
+
+<div class="movement__box">
+  <h4 class="movement__title">Rotate</h4>
+  <LiniarEasing type={"rotate"} />
+  <div class="container">
+    <h3 class="curent__title">{currentEasingType?.title || "custom"}</h3>
+    <div class="playground">
+      <div class="box__curent" style="--bize: {curveCSS}"></div>
+    </div>
+  </div>
+</div>
+
+<style>
+  .movement__title {
+    font-weight: bold;
+    font-size: 40px;
+    font-family: HelveticaNowDisplay, "Helvetica Neue Light", "Helvetica Neue",
+      Helvetica, Arial, "Lucida Grande", sans-serif;
+    background-color: #ffff;
+    padding: 8px;
+    text-decoration: none;
+    margin-block-end: 10px;
+  }
+  .movement__box {
+    border: 1px solid #ffc300;
+    padding: 20px;
+    margin-block-end: 20px;
+  }
+  .curent__title {
+    font-weight: bold;
+    font-size: 40px;
+    font-family: HelveticaNowDisplay, "Helvetica Neue Light", "Helvetica Neue",
+      Helvetica, Arial, "Lucida Grande", sans-serif;
+    color: rgb(255, 255, 255);
+    text-decoration: none;
+    margin-block-end: 10px;
+  }
+  .container {
+    margin-block-end: 20px;
+  }
+  .box__curent {
+    --size: 50px;
+    width: var(--size);
+    height: var(--size);
+    border: 2px solid tomato;
+    animation: move__curent 1500ms var(--bize) infinite;
+  }
+  .playground {
+    width: 300px;
+    height: 300px;
+    display: grid;
+    place-items: center;
+    padding-block: 20px;
+    padding-inline: 10px;
+    border: 1px solid var(--lighterGray);
+    background: repeating-linear-gradient(
+        to bottom,
+        hsl(182, 43%, 76%, 0.4),
+        hsl(182, 43%, 76%, 0.4) 1px,
+        transparent 1px,
+        transparent 10.14%
+      ),
+      repeating-linear-gradient(
+        to right,
+        hsl(182, 43%, 76%, 0.4),
+        hsl(182, 43%, 76%, 0.4) 1px,
+        transparent 1px,
+        transparent 10.14%
+      );
+    border-radius: 10px;
+  }
+
+  @keyframes move__curent {
+    0% {
+      transform: rotate(0deg);
+    }
+
+    30% {
+      transform: rotate(90deg);
+    }
+    70% {
+      transform: rotate(180deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+</style>
