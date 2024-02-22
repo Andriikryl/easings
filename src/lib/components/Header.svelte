fa55bcaa4963060eb6e7de350ce002466c92d7a3
Andriikryl <andreyk557@gmail.com>
2024-02-22 11:44:11 +0200

    add Header styles new

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -12,9 +12,13 @@
 
 <style>
   .header {
+    position: sticky;
+    top: 0;
     padding-block: 20px;
     border-bottom: 1px solid black;
+    background-color: #f1faee;
     margin-block-end: 20px;
+    z-index: 100;
   }
   .name {
     text-transform: uppercase;
