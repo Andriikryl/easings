660db1f7d3f274911c916eece1e73850d0ee697a
Andriikryl <andreyk557@gmail.com>
2024-02-22 16:29:21 +0200

    add new styles for footer component

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,28 +1,30 @@
 <script>
-    import Container from "./Container.svelte";
-  </script>
-  
-  <footer class="footer">
-    <Container>
-      <div class="header__box">
-        <p class="name">footer</p>
-      </div>
-    </Container>
-  </footer>
-  
-  <style>
-    .footer {
-      margin-block-start: 20px;
-      padding-block: 20px;
-      border-top: 1px solid black;
-    }
-    .name {
-      text-transform: uppercase;
-      color: var(--black, #1b1b1b);
-      font-family: "Patrick Hand";
-      font-size: 22px;
-      font-style: normal;
-      font-weight: 400;
-      line-height: 28px; /* 127.273% */
-    }
-  </style>
+  import Container from "./Container.svelte";
+</script>
+
+<footer class="footer">
+  <Container>
+    <div class="header__box">
+      <p class="name">footer</p>
+    </div>
+  </Container>
+</footer>
+
+<style>
+  .footer {
+    padding-block: 20px;
+    border-bottom: 1px solid black;
+    background-color: #f1faee;
+    margin-block-end: 20px;
+    z-index: 100;
+  }
+  .name {
+    text-transform: uppercase;
+    color: var(--black, #1b1b1b);
+    font-family: "Patrick Hand";
+    font-size: 22px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 28px; /* 127.273% */
+  }
+</style>
